d6b884ead5cd131a282fab21e93d9c486bf72ee9
Abolfazl Karimi <abolfazl.karimi@gmail.com>
2023-05-16 12:09:03 -0400

    Adding RSS python module to Carla's Docker image.

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -2,10 +2,16 @@
 pip install cython
 pip install numpy scipy pybind11 wheel
 
+# Set up Carla python module
+CARLA_ROOT='/home/carla'
+carla_egg=carla-0.9.13-py3.7-linux-x86_64.egg
+PYTHONPATH="${CARLA_ROOT}/PythonAPI/carla/:${CARLA_ROOT}/PythonAPI/carla/agents/:${CARLA_ROOT}/PythonAPI/carla/dist/${carla_egg}"
+echo "export PYTHONPATH=${PYTHONPATH}" >> .bashrc
+
 cd /home/carla/Scenic
 pip install -e .
 
 cd /home/carla/ScenarioGeneration
 pip install -r requirements.txt
 
-/bin/bash "$@"
+/bin/bash "$@" 
