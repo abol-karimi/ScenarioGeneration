915aa84d01b94c5ac6ddecbfdb9a567c66f0fd94
Abolfazl Karimi <abolfazl.karimi@gmail.com>
2023-05-31 16:35:41 -0400

    Abstract definition of experiments for the research questions.

---
+++ experiments/SUT_vs_surrogate.py
@@ -0,0 +1,8 @@
+"""
+Research question:
+  How does the performance of a fuzzer compare
+  under simulating the SUT vs a much simpler surrogate,
+  in terms of number of accidents for a fixed computation budget?
+
+A surrogate is a model of the SUT that is much faster to simulate.
+"""
---
+++ experiments/our-fuzzer_vs_existing-fuzzers.py
@@ -0,0 +1,12 @@
+"""
+Research question:
+  How does the performance of our fuzzer compares to one or more existing fuzzers
+  in terms of number of accidents for a fixed computation budget?
+
+Our fuzzer:
+Specialized for scenario-generation.
+
+Existing fuzzers:
+1. AFL fuzzer
+2. 
+"""
