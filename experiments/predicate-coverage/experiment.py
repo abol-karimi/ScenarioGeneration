56b02d300c4c3dc192b936f8910e0a9b8fcb7e18
Abolfazl Karimi <abolfazl.karimi@gmail.com>
2023-06-15 09:49:18 -0400

    The predicate-coverage experiment abstract.

---
+++ experiments/predicate-coverage/experiment.py
@@ -0,0 +1,11 @@
+# System Under Test:
+#  A VUT (Vehicle Under Test) following a predefined route
+
+# Test coverage criteria:
+#  Predicate coverage
+
+# Experiment hypotethis:
+#  None of the available fuzzers acheive good coverage
+
+# Available fuzzers:
+#  AFL fuzzer
