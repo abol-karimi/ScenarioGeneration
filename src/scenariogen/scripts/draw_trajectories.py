78e25c9140ffbc76b41ec383a587145d053fd804
Abolfazl Karimi <abolfazl.karimi@gmail.com>
2023-07-07 09:15:47 -0400

    Bug in draw_trajectories.py after adding heading in saved simulation trajectories

--- src/scenariogen/scripts/draw_trajectories.py
+++ src/scenariogen/scripts/draw_trajectories.py
@@ -13,31 +13,13 @@ from scenariogen.core.utils import sample_trajectory
 
 parser = argparse.ArgumentParser(description='play the given scenario.')
 parser.add_argument('seed_path', help='relative path of the seed')
-parser.add_argument('--timestep', type=float, default=0.05,
-                    help='length of each simulation step, controls replay speed.')
-parser.add_argument('--lifetime', type=float,
-                    help='how long till drawings are erased')
-duration = parser.add_mutually_exclusive_group()
-duration.add_argument('--steps', type=int, help='max number of steps to replay')
-duration.add_argument('--seconds', type=float, help='max seconds to replay')
+parser.add_argument('--lifetime', type=float, default=20,
+                    help='how long the drawings last')
 args = parser.parse_args()
 
 with open(args.seed_path, 'r') as f:
     seed = jsonpickle.decode(f.read())
     assert isinstance(seed, Seed)
-
-# Default duration is the whole scenario:
-steps = seed.trajectories[0].ctrlpts[-1][2]//args.timestep
-# Restric the duration if requested:
-if args.steps:
-    steps = min(steps, args.steps)
-elif args.seconds:
-    steps = min(steps, args.seconds*args.timestep)
-
-if args.lifetime:
-   lifetime = args.lifetime
-else:
-   lifetime = steps*args.timestep
 
 client = carla.Client('127.0.0.1', 2000)
 world = client.get_world()
@@ -52,25 +34,28 @@ seed_path = Path(args.seed_path)
 with open(seed_path.parents[1]/'initial_seeds_definitions'/f'{seed_path.stem}_sim_trajectories.pickle', 'rb') as f:
     sim_trajectories = pickle.load(f)
 
-#--- Draw the simulated trajectories
+#--- Draw the simulated trajectories, in green
 for tj in sim_trajectories:
   for p in tj:
     visualization.draw_point(world,
-                             p,
+                             (p[0], p[1], p[3]),
                              size=0.1, 
                              color=carla.Color(0, 255, 0),
-                             lifetime=lifetime)
+                             lifetime=args.lifetime)
 
-#--- Draw the spline approximation of the trajectories
+#--- Draw the spline approximation of the trajectories, in blue
+resolution = 0.05
+interval = (0, seed.trajectories[0].ctrlpts[-1][2])
+steps = int(interval[1] // resolution)
 for spline in seed.trajectories:
   traj_sample = sample_trajectory(spline, 
-                                  int(steps)+1,
-                                  0, 
-                                  args.timestep*steps)
+                                  steps+1,
+                                  interval[0], 
+                                  interval[1])
   for i, p in enumerate(traj_sample):
     visualization.draw_point(world,
                              p, 
-                             i*args.timestep, 
+                             i*resolution, 
                              0.1, 
                              carla.Color(0, 0, 255),
-                             lifetime)
+                             args.lifetime)
