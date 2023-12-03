59269fef879e393cddf6564f6f2cdc6fdb587928
Abolfazl Karimi <abolfazl.karimi@gmail.com>
2023-12-02 22:13:11 -0500

    Instead of copying, mount Carla's PythonAPI to docker image, for debugging.

--- docker_container.sh
+++ docker_container.sh
@@ -5,6 +5,7 @@ sudo docker run --rm -it --privileged \
 -e SDL_VIDEODRIVER=x11 \
 -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
 --mount type=bind,source=$HOME/ScenarioGeneration,target=/home/carla/ScenarioGeneration \
+--mount type=bind,source=$HOME/ScenarioGeneration/Docker/dev/PythonAPI,target=/home/carla/PythonAPI \
 --mount type=bind,source=$HOME/Scenic_10-03-2023,target=/home/carla/Scenic \
 --mount type=bind,source=$HOME/leaderboard,target=/home/carla/leaderboard \
 --mount type=bind,source=$HOME/scenario_runner,target=/home/carla/scenario_runner \
