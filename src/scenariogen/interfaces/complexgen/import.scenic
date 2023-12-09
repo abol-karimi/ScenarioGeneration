0fe18c156eaa2b39b126325d65b69f178210207d
Abolfazl Karimi <abolfazl.karimi@gmail.com>
2023-12-09 12:20:51 -0500

    Interface to import scenarios from ScenarioComplexity.

---
+++ src/scenariogen/interfaces/complexgen/import.scenic
@@ -0,0 +1,83 @@
+""" Scenario Description
+Replay complexgen's scenario without the ego agents ('ego' and 'illega').
+"""
+param map = localPath('/home/carla/CarlaUE4/Content/Carla/Maps/OpenDrive/Town05.xodr')  # or other CARLA map that definitely works
+param carla_map = 'Town05'
+model scenic.simulators.carla.model
+
+import jsonpickle
+param complexgen_scenario_path = None
+with open(globalParameters.caller_config['complexgen_scenario_path'], 'r') as f:
+  scenario = jsonpickle.decode(f.read())
+param timestep = scenario.timestep
+param steps = scenario.maxSteps
+
+intersection = network.elements[scenario.intersection_uid]
+blueprints = scenario.blueprints
+events = scenario.events
+curves = scenario.curves
+sim_trajs = scenario.sim_trajectories
+sample_size = int(scenario.maxSteps)+1
+
+from complexgen.core.utils import curves_to_trajectories
+trajectory = curves_to_trajectories(curves, sim_trajs, sample_size)
+
+from scenariogen.core.geometry import CurvilinearTransform
+ego_lanes = tuple(network.elements[uid] for uid in scenario.maneuver_uid['ego'])
+ego_route = tuple(l.uid for l in ego_lanes)
+ego_p0 = trajectory['ego'][0][0]
+transform = CurvilinearTransform([p for lane in ego_lanes
+                                    for p in lane.centerline.lineString.coords
+                                    ])
+ego_init_progress_x = transform.curvilinear(ego_p0)[0]
+
+config = {'description': 'A scenario imported from complexgen',
+          'carla_map': scenario.map_name,
+          'map': scenario.map_path,
+          'weather': scenario.weather,
+          'timestep': scenario.timestep,
+          'steps': globalParameters.steps,
+          'intersection': scenario.intersection_uid,
+          'ego_blueprint': scenario.blueprints['ego'],
+          'ego_route': ego_route,
+          'ego_init_progress_ratio': ego_init_progress_x/transform.axis.length,
+          }
+
+import complexgen.simulators.carla.visualization as visualization
+from complexgen.core.signals import SignalType
+
+car2time2signal = {car:{e.frame:e.signal for e in es if e.name == 'signaledAtForkAtTime'} 
+	for car, es in events.items()}
+
+behavior ReplayBehavior():
+	carla_world = simulation().world
+	while True:
+		t = simulation().currentTime
+		state = trajectory[self.name][t]
+		take SetPoseAction(state[0], state[1])
+
+		if car2time2signal[self.name].__contains__(t):
+			lights = SignalType[car2time2signal[self.name][t].upper()].to_vehicleLightState()
+			take SetVehicleLightStateAction(lights)
+
+		visualization.label_car(carla_world, self)
+
+scenario SeedScenario():
+  setup:
+    with open('src/scenariogen/simulators/carla/blueprint2dims_cars.json', 'r') as f:
+      blueprint_lib = jsonpickle.decode(f.read())
+
+    for carName, traj in trajectory.items():
+      if carName in {'ego', 'illegal'}:
+        continue
+      carState = traj[0]
+      car = new Car at carState[0], facing carState[1],
+        with name carName,
+        with blueprint blueprints[carName],
+        with length blueprint_lib[blueprints[carName]]['length'],
+        with width blueprint_lib[blueprints[carName]]['width'],
+        with color Color(0, 0, 1),
+        with route scenario.maneuver_uid[carName],
+        with physics False,
+        with allowCollisions False,
+        with behavior ReplayBehavior()
---
+++ src/scenariogen/scripts/import.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3.8
+
+# Standard libraries
+import argparse
+import jsonpickle
+from pathlib import Path
+
+# Scenic modules
+import scenic
+scenic.setDebuggingOptions(verbosity=0, fullBacktrace=True)
+from scenic.core.simulators import SimulationCreationError
+from scenic.core.dynamics import GuardViolation
+
+
+# My modules
+from scenariogen.core.utils import seed_from_sim
+from scenariogen.core.errors import NonegoCollisionError
+
+#----------Main Script----------
+parser = argparse.ArgumentParser(description='Make a seed from a scenic scenario.')
+parser.add_argument('complexgen_scenario_path',
+                    help='Path of the ComplexGen scenario file (in json format)')
+parser.add_argument('--simulator', choices=['newtonian', 'carla'], default='carla',
+                    help='The simulator')
+parser.add_argument('--render_spectator', action='store_true',
+                    help='render a spectator above the intersection')
+parser.add_argument('--render_ego', action='store_true',
+                    help='render ego viewpoint (only in the Carla simulator)')
+parser.add_argument('--out_path',
+                    help='Path where the generated seed will be stored')
+parser.add_argument('--spline_degree', default = 3, type=int)
+parser.add_argument('--spline_knots_size', default = 50, type=int)
+args = parser.parse_args()
+
+# Run the scenario
+scenic_scenario = scenic.scenarioFromFile(
+                    f'src/scenariogen/simulators/{args.simulator}/create.scenic',
+                    mode2D=True,
+                    params = {'caller_config':{'scenario_path': 'src/scenariogen/interfaces/complexgen/import.scenic',
+                                               'complexgen_scenario_path': args.complexgen_scenario_path,
+                                               'render_spectator': args.render_spectator,
+                                               'render_ego': args.render_ego,
+                                               }
+                              }
+                    )
+scene, _ = scenic_scenario.generate(maxIterations=1)
+simulator = scenic_scenario.getSimulator()
+if args.simulator == 'carla' and not args.render_spectator:
+    settings = simulator.world.get_settings()
+    settings.no_rendering_mode = True
+    simulator.world.apply_settings(settings)
+
+try:
+    sim_result = simulator.simulate(
+                    scene,
+                    maxSteps=scenic_scenario.params['steps'],
+                    maxIterations=1,
+                    raiseGuardViolations=True
+                    )
+    if sim_result is None:
+        raise RuntimeError('Simulation rejected.')
+except NonegoCollisionError as err:
+    print(f'Collision between nonego {err.nonego} and actor {err.other}, discarding the simulation.')
+    exit()
+except SimulationCreationError:
+    print('Failed to create scenario.')
+    exit()
+else:
+    # Save the seed
+    scenario_path = Path(args.complexgen_scenario_path)
+    seed = seed_from_sim(sim_result,
+                        scenic_scenario.params['timestep'],
+                        degree=args.spline_degree,
+                        knots_size=args.spline_knots_size)
+    if args.out_path:
+        with open(args.out_path, 'w') as f:
+            f.write(jsonpickle.encode(seed, indent=1))
+    else:
+        with open(f'experiments/seeds/complex/seeds/{scenario_path.stem}.json', 'w') as f:
+            f.write(jsonpickle.encode(seed, indent=1))
