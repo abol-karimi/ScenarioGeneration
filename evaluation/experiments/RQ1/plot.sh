9a3688528edb7447eb1cc23445a1e46ad66b0420
Abolfazl Karimi <25490389+abol-karimi@users.noreply.github.com>
2024-04-29 21:55:04 -0400

    Plotting scripts for RQ1

---
+++ evaluation/experiments/RQ1/plot.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ScenariogenDependencies=/users/a/b/abol
+CARLA_Dist=/work/users/a/b/abol/bionic/carla/Dist/CARLA_Shipping_0.9.15-169-g063cc9d90/LinuxNoEditor
+
+module add apptainer/1.3.0-1
+
+apptainer run \
+    --nv \
+    --cleanenv \
+    --bind ${CARLA_Dist}:/home/scenariogen/carla \
+    --bind ${ScenariogenDependencies}/Scenic_04-10-2024:/home/scenariogen/Scenic \
+    --bind ${ScenariogenDependencies}/ScenarioGeneration:/home/scenariogen/ScenarioGeneration \
+    --bind ${ScenariogenDependencies}/ScenarioComplexity:/home/scenariogen/ScenarioComplexity \
+    --bind ${ScenariogenDependencies}/z3-4.12.6-x64-glibc-2.35:/home/scenariogen/z3 \
+    --bind ${ScenariogenDependencies}/carla_garage_fork:/home/scenariogen/carla_garage_fork \
+    ${ScenariogenDependencies}/ScenarioGeneration/Longleaf/bionic/scenariogen/scenariogen.sif \
+        evaluation/experiments/RQ1/plot.py
