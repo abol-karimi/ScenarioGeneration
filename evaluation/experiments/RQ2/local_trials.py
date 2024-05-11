5cb8515457cf47a827f36bf8775b5b20d58aa80c
Abolfazl Karimi <25490389+abol-karimi@users.noreply.github.com>
2024-05-10 22:53:09 -0400

    Scripts for running RQ2 trials locally

---
+++ evaluation/experiments/RQ2/local_trials.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import subprocess
+from itertools import product, permutations
+from datetime import timedelta
+import os
+
+generators = ('Random', 'PCGF', )
+egos = ('autopilot', 'BehaviorAgent', 'TFPP')
+randomizer_seeds = (0, 1, )
+coverages = ('traffic-rules', )
+trial_timeout = timedelta(minutes=10) # the same as the time budget for generating the test cases
+
+# For RQ2, we only vary the ego between the test-case-generation trials and
+# and the test-case-execution trials.
+trials = product(generators, permutations(egos, r=2), randomizer_seeds, coverages)
+STORE_BASE_DIR = os.environ.get('STORE_BASE_DIR')
+
+for generator, (gen_ego, test_ego), randomizer_seed, coverage in trials:
+    cmd = f'''
+        {STORE_BASE_DIR}/ScenarioGeneration/Apptainer/scripts.sh scenariogen_run bionic Shipping \
+            evaluation/experiments/RQ2/trial.py \
+            --ego {test_ego} \
+            --seeds-folder evaluation/results/RQ1/{generator}_{gen_ego}_{coverage}/{randomizer_seed}/fuzz-inputs \
+            --coverage {coverage} \
+            --seconds {trial_timeout.total_seconds()} \
+            --output-folder evaluation/results/RQ2/{generator}_{gen_ego}_{coverage}/{randomizer_seed}/{test_ego}
+    '''
+    completed_proc = subprocess.run(cmd, shell=True)
+    print(f'Completed trial {generator}_{gen_ego}_{coverage}_{randomizer_seed}_{test_ego} with exit code: {completed_proc.returncode}')
+
---
+++ evaluation/experiments/RQ2/trial.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import setproctitle
+import argparse
+from pathlib import Path
+
+import evaluation.experiments.Test as Test_experiment
+import evaluation.utils.experiment_runner
+import scenariogen.core.logging.server as log_server
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Run a trial for the PCGF-vs-baselines experiment.')
+    parser.add_argument('--ego',
+                        help='the agent under test')
+    parser.add_argument('--seeds-folder', required=True,
+                        help='the test-case corpus')
+    parser.add_argument('--coverage',
+                        help='the coverage monitor to compute the coverage of the test-case corpus')
+    parser.add_argument('--seconds', type=float, required=True,
+                        help='number of seconds to run the trial')
+    parser.add_argument('--output-folder', required=True,
+                        help='the generator used to generate the test-case corpus')
+    args = parser.parse_args()
+
+    setproctitle.setproctitle(f'RQ2_{args.output_folder}_{args.ego}')
+
+    # Run the experiment
+    trial_output_path = Path(args.output_folder)
+    trial_output_path.mkdir(parents=True, exist_ok=True)
+
+    config = Test_experiment.get_config(args.ego,
+                                       args.coverage,
+                                       args.seeds_folder,
+                                       args.seconds,
+                                       args.output_folder)
+    evaluation.utils.experiment_runner.run(config)
