84bd1e991e1be288c879d39bb156fb97e45f9e63
Abolfazl Karimi <25490389+abol-karimi@users.noreply.github.com>
2024-05-12 17:32:28 -0400

    Multiprocess plot script for RQ1 results

--- evaluation/experiments/RQ1/plot.py
+++ evaluation/experiments/RQ1/plot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from itertools import product
+import multiprocessing
 
 import evaluation.utils.plots.baselines_vs_PCGF_per_time
 import evaluation.utils.plots.baselines_vs_PCGF_per_fuzz_input
@@ -8,7 +9,7 @@ import evaluation.utils.plots.baselines_vs_PCGF_per_fuzz_input
 
 if __name__ == '__main__':
 
-    generators = ('Atheris', 'PCGF', 'Random')
+    generators = ('PCGF', 'Atheris', 'Random')
     egos = ('autopilot', 'BehaviorAgent', 'TFPP')
     coverages = ('traffic-rules', )
     RQ1_folder = f'evaluation/results/RQ1'
@@ -40,16 +41,30 @@ if __name__ == '__main__':
     # each trial's results can be assessed with different metrics
     assessments = product(normalizers, coverage_filters)
 
+    spawn_ctx = multiprocessing.get_context('spawn')
+    processes = []
+
     # for each (trial, assessment) combination, we generate a separate figure;
     # for each coverage type, we generate a separate subplot in the figure
     coverage_types = ('statementSet', 'statement', 'predicateSet', 'predicate')
     for (ego, coverage), (normalizer, coverage_filter) in product(trials, assessments):
-        coverage_files = (f'{RQ1_folder}/{g}_{ego}_{coverage}/{coverage_filter}-coverage.json'
-                            for g in generators)
+        coverage_files = tuple(f'{RQ1_folder}/{g}_{ego}_{coverage}/{coverage_filter}-coverage.json'
+                                for g in generators)
         output_file = f'{RQ1_folder}/{ego}_{coverage}_{coverage_filter}_{normalizer}.png'
 
-        plotter[normalizer].plot(coverage_files,
-                                 (colors[g] for g in generators),
-                                 (labels[g] for g in generators),
-                                 coverage_types,
-                                 output_file)
+        plot_process = multiprocessing.Process(target=plotter[normalizer].plot,
+                                                args=(coverage_files,
+                                                    (colors[g] for g in generators),
+                                                    (labels[g] for g in generators),
+                                                    coverage_types,
+                                                    output_file),
+                                                name=output_file,
+                                                daemon=False
+                                                )
+
+        plot_process.start()
+        processes.append(plot_process)
+    
+    for p in processes:
+        p.join()
+        print(f'{p.name} exited with exitcode {p.exitcode} .')
